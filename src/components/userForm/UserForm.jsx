6bcb5eeb68b4d6e10968b8d7f4c95637f02a6500
alan <alanarostegui@hotmail.com>
2020-06-06 21:04:30 -0500

    feature(app): add and create suer form and view list components

---
+++ src/components/userForm/UserForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component {
+    render() { 
+        return ( 
+            'View List'
+         );
+    }
+}
+export default UserForm;
---
+++ src/components/viewList/ViewList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ViewList extends Component {
+    render() { 
+        return ( 
+            'View List'
+         );
+    }
+}
+export default ViewList;
